b4c0516a4d5f981fa216771bcd49cf98ef30bc33
sai-krishna-sk <148880582+sai-krishna-sk@users.noreply.github.com>
2025-03-11 22:09:33 +0530

    Update index.js

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -94,8 +94,8 @@ export default function Home() {
         
         {/* Hero Section */}
         <div className="relative h-160 w-full overflow-hidden">
-          <div className="absolute inset-0 bg-black/60 z-10"></div>
-          <div className="absolute inset-0 bg-[url('https://img.freepik.com/premium-photo/luxury-jewelry-black-friday-advertisement-commercial-photography_950002-326749.jpg?w=1060')] bg-cover bg-center opacity-80"></div>
+          <div className="absolute inset-0 bg-black/50 z-10"></div>
+          <div className="absolute inset-0 bg-[url('https://raw.githubusercontent.com/sai-krishna-sk/e-commerce/refs/heads/main/public/images/DeWatermark.ai_1741710171184.png')] bg-cover bg-center opacity-90"></div>
           <div className="relative z-20 h-full flex flex-col items-center justify-center text-center px-4">
             <h1 className="text-4xl md:text-5xl font-serif text-white-800 mb-4">TIMELESS ELEGANCE</h1>
             <p className="text-xl text-white-300 max-w-2xl">Discover our exquisite collection of handcrafted jewelry pieces</p>
